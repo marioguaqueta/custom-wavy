91759d0a78f64fa2d337e0cfcdc6c4c9112c82eb
Mario Guaqueta Restrepo <m_a15@hotmail.es>
2019-10-23 14:34:04 -0300

    New fixes

--- index.js
+++ index.js
@@ -144,12 +144,12 @@ define( function( require ) {
 
     function onGetTokens (tokens) {
         //Response: tokens = { token: <legacy token>, fuel2token: <fuel api token> }
-        //console.log(tokens);
+        console.log(tokens);
     }
 
     function onGetEndpoints (endpoints) {
         //Response: endpoints = { restHost: <url> } i.e. "rest.s1.qa1.exacttarget.com"
-        //console.log(endpoints);
+        console.log(endpoints);
     }
 
     function onClickedNext () {
